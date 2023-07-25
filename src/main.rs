f42d4b3ad41c5c05d1a662eaa9194a23b07ddedf
L-Jac <3137501630@qq.com>
2023-07-25 11:21:35 +0800

    finish all

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use class::state_machine;
-
-mod class {
-    pub mod correspondence;
-    pub mod detector;
-    pub mod knn_tracker;
-    pub mod parameters_processing;
+mod module {
+    mod correspondence;
+    mod detector;
+    mod knn_tracker;
+    mod parameters_processing;
     pub mod state_machine;
-    pub mod webcamstream;
+    mod webcamstream;
 }
 
+use module::state_machine::StateMachine;
+
 fn main() {
-    let mut project = state_machine::StateMachine::new();
+    let mut project = StateMachine::new();
     loop {
         project.working();
     }
